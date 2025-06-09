4af483cca95074dc6b6ba2b8760b84e07d2bc6c7
Francisco Novais <fjnovais.zoot@gmail.com>
2025-06-09 13:47:59 -0600

    Create 2025-03-05-CANPSci.ml

---
+++ _publications/2025-03-05-CANPSci.ml
@@ -0,0 +1,15 @@
+---
+title: "Comparative identification of common Aspen Parkland plant species using three molecular barcodes"
+collection: publications
+permalink: /publication/2025-03-05-CANPSci
+date: 03 June 2025
+venue: '<span style="color:#B10E06">Canadian Journal of Plant Science</span>'
+
+citation: 'Valentine Udeh, Francisco De Novais, Lisa Raatz, James Cahill, Leluo Guan, Eric G Lamb, Carolyn Fitzsimmons, and Edward W. Bork.Comparative identification of common Aspen Parkland plant species using three molecular barcodes. Canadian Journal of Plant Science (2025). https://doi.org/10.1139/cjps-2025-0031'
+---
+
+[Access paper here](https://doi.org/10.1139/cjps-2025-0031)
+
+<b>Abstract</b>
+
+This study evaluated the effectiveness of three chloroplast barcodes (trnL, rbcL, and matK) for identifying 14 co-occurring plant species representing six common families of plants in the Aspen Parkland of Alberta. Maximum likelihood phylogenetic analysis and genetic distancing showed that matK exhibited the highest within-family genetic distance but lacked universal primers. While rbcL was easily amplified, it had lower discriminatory power. In contrast, trnL offered superior within-family species discrimination and possesses a shorter fragment that can be amplified, which lends it suitable for Next Generation sequencing of degraded plant DNA in fecal samples of herbivores.
